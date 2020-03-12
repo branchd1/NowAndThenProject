b46ba1612a180a5a4c08ab1183361b0f0934e126
Jeff Waters <2495819w@student.gla.ac.uk>
2020-03-12 23:33:24 +0000

    Added Pictures to Admin

--- nowandthen/admin.py
+++ nowandthen/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from nowandthen.models import Category, Page
+from nowandthen.models import Category, Page, Pictures
 from nowandthen.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
 
+class PicturesAdmin(admin.ModelAdmin):
+    list_display = ('image', 'title', 'description', 'tag_one', 'tag_two','era',)
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
+admin.site.register(Pictures, PicturesAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
