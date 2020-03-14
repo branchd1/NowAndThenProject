299a521fa414e2668112ff997e6bfe408e2a3479
Jeff Waters <2495819w@student.gla.ac.uk>
2020-03-14 22:57:15 +0000

    Corrected error

--- nowandthen/views.py
+++ nowandthen/views.py
@@ -20,7 +20,7 @@ from datetime import datetime
 #Method adapted from https://stackoverflow.com/questions/45226024/how-can-i-make-categories-in-a-image-gallery-in-django?fbclid=IwAR2-p5CeVAdPdpiYCJTDRlCW5-M_0AOMmKNygNBoEpv6v498Km8sySuFO3o
 
 def photo_list(request):
-    queryset = Photo.objects.all()
+    queryset = Picture.objects.all()
     context = {
     "photos": queryset,
 }
